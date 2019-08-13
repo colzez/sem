94411352996186f54f4c0faf0f94fe12b37e5ffa
colzez <htoomyataung541@gmail.com>
2019-08-13 20:40:43 +0630

    red velvet

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,10 +1,26 @@
-
 package com.napier.sem;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoCollection;
+import org.bson.Document;
 
 public class App
 {
     public static void main(String[] args)
     {
-        System.out.println("Boo yah! Song name-Happiness");
+        MongoClient mongoClient = new MongoClient("localhost", 27000);
+        MongoDatabase database = mongoClient.getDatabase("mydb");
+        MongoCollection<Document> collection = database.getCollection("test");
+        Document doc = new Document("name", "Kevin Chalmers")
+                .append("class", "Software Engineering Methods")
+                .append("year", "2018/19")
+                .append("result", new Document("CW", 95).append("EX", 85));
+        // Add document to collection
+        collection.insertOne(doc);
+
+        // Check document in collection
+        Document myDoc = collection.find().first();
+        System.out.println(myDoc.toJson());
     }
 }
